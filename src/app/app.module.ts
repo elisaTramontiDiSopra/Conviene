cb64b1e3be0aae600a8662293064d15e65565cbe
elisaTramontiDiSopra <elisa.tramontidisopra@gmail.com>
2017-11-05 19:26:45 +0100

    working firebase injection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,19 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { AngularFireModule } from "angularfire2"
+import { AngularFireDatabase, FirebaseListObservable } from "angularfire2/database"
+
+// AF2 Settings
+export const firebaseConfig = {
+  apiKey: "AIzaSyCc4wpmOFvjihiU3exEBeiI6ST1bebbbBI",
+  authDomain: "convienetest.firebaseapp.com",
+  databaseURL: "https://convienetest.firebaseio.com",
+  projectId: "convienetest",
+  storageBucket: "convienetest.appspot.com",
+  messagingSenderId: "1018324230887"
+};
+
 @NgModule({
   declarations: [
     MyApp,
@@ -14,6 +27,7 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -23,6 +37,7 @@ import { HomePage } from '../pages/home/home';
   ],
   providers: [
     StatusBar,
+    AngularFireDatabase,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
